b42daaf9f8d43f684f32f4523b117388bd95bb6a
Theo-projects <uzuntudor@yahoo.com>
2019-09-02 18:09:24 +0300

    New changes
    then i'll try javascript tests

---
+++ FFW Test/FFW JS tests.js
@@ -0,0 +1,7 @@
+let url = new URL("https://my-ffw-example.com/dir/subdir/file.html#hash");
+
+// this Return 
+// { hash:"#hash", host:"my-ffw-example.com", hostname:"my-ffw-example.com", 
+// href:"http://my-ffw-example.com/dir/subdir/file.html#hash", origin:"http://my-ffw-example.com", 
+// password:"", pathname:"/dir/subdir/file.html", 
+// port:"", protocol:"http:", search: "", username: "" }

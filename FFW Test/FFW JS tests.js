9c4f100c3ec027a246937acf98fec61499cebef4
Theo-projects <uzuntudor@yahoo.com>
2019-09-03 20:03:34 +0300

    minor changes

--- FFW Test/FFW JS tests.js
+++ FFW Test/FFW JS tests.js
@@ -20,7 +20,7 @@ let url = new URL("https://my-ffw-example.com/dir/subdir/file.html#hash");
 
 // this Return 
 // { hash:"#hash", host:"my-ffw-example.com", hostname:"my-ffw-example.com", 
-// href:"http://my-ffw-example.com/dir/subdir/file.html#hash", origin:"http://my-ffw-example.com", 
+// href:"https://my-ffw-example.com/dir/subdir/file.html#hash", origin:"https://my-ffw-example.com", 
 // password:"", pathname:"/dir/subdir/file.html", 
 // port:"", protocol:"http:", search: "", username: "" }
 
@@ -42,7 +42,7 @@ console.log(names);
 let enrolled = data.enrolled
 function empty(enrolled)
 {
-  if(this.enrolled = !this.enrolled)
+  if(enrolled = !enrolled)
   { 
     return false; 
   }
@@ -63,3 +63,24 @@ console.log(marks);
 $(".articles .node");
 $(".articles div");
 $("div.node");
+
+
+// 6-th question
+
+document.write(user.jason); // Output: user
+document.write(user.jason.name); // Output: name
+document.write(user.jason.e_mail); // Output: e_mail
+
+// 7-th question
+
+// componentWillMount
+// componentWillReceiveProps
+// componentWillUpdate
+// Because of this, we will be adding an “UNSAFE_” prefix to these lifecycles
+
+// 8-th question
+
+// button 1 is equal to button 4 and button 2 with 3
+// When we click button 1 , it calls alert(this.text) wich is 'This is an example text' will appear as alert.
+// handler  is equal with handler 3 so => button 1 = button 4
+// When we click button 2 handler 1 returns handler 2 , so => button 2 = button 3 

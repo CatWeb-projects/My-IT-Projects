806ed62b73e0cc17fdd1659a552af363e3c23f04
Theo-projects <uzuntudor@yahoo.com>
2019-09-17 17:44:32 +0300

    react submit form

--- React Tutorial/src/index.js
+++ React Tutorial/src/index.js
@@ -1,33 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Header extends React.Component {
+class MyForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {favoritecolor: "red"};
+    this.state = {
+      username: '',
+      age: null,
+    };
   }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({favoritecolor: "yellow"})
-    }, 1000)
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    let age = this.state.age;
+    if (!Number(age)) {
+      alert("Your age must be a number");
+    }
   }
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    document.getElementById("div1").innerHTML =
-    "Before the update, the favorite was " + prevState.favoritecolor;
-  }
-  componentDidUpdate() {
-    document.getElementById("div2").innerHTML =
-    "The updated favorite is " + this.state.favoritecolor;
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
   }
   render() {
     return (
-      <div>
-      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-      <div id="div1"></div>
-      <div id="div2"></div>
-      </div>
+      <form onSubmit={this.mySubmitHandler}>
+      <h1>Hello {this.state.username} {this.state.age}</h1>
+      <p>Enter your name:</p>
+      <input
+        type='text'
+        name='username'
+        onChange={this.myChangeHandler}
+      />
+      <p>Enter your age:</p>
+      <input
+        type='text'
+        name='age'
+        onChange={this.myChangeHandler}
+      />
+      <br/>
+      <br/>
+      <input type='submit' />
+      </form>
     );
   }
 }
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+ReactDOM.render(<MyForm />, document.getElementById('root'));

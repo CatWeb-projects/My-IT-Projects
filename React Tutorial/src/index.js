eddf120ea2772d97ea57c41b59aef6ae3eb3776f
Theo-projects <uzuntudor@yahoo.com>
2019-09-17 14:32:22 +0300

    react life cycle

--- React Tutorial/src/index.js
+++ React Tutorial/src/index.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Car extends React.Component {
+class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      brand: "Hyundai",
-      model: "Tucson",
-      color: "black",
-      year: 2016
-    };
+    this.state = {favoritecolor: "red"};
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({favoritecolor: "yellow"})
+    }, 1000)
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    document.getElementById("div1").innerHTML =
+    "Before the update, the favorite was " + prevState.favoritecolor;
+  }
+  componentDidUpdate() {
+    document.getElementById("div2").innerHTML =
+    "The updated favorite is " + this.state.favoritecolor;
   }
   render() {
     return (
       <div>
-        <h1>My {this.state.brand}</h1>
-        <p>
-          It is a {this.state.color}&nbsp;
-          {this.state.model}&nbsp;
-          from {this.state.year}.
-        </p>
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <div id="div1"></div>
+      <div id="div2"></div>
       </div>
     );
   }
 }
 
-ReactDOM.render(<Car />, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('root'));

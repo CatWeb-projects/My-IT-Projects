8af295eced1cf8ff4591bc88ec0d36f7dec8220a
Theo-projects <uzuntudor@yahoo.com>
2020-05-22 16:06:14 +0300

    EMP Project 1.0.0
    Need to bug fix

---
+++ EnergyMarketPrice/JS/index.js
@@ -0,0 +1,37 @@
+let checkButton = document.querySelector('#click-switcher', '.click-it').addEventListener('click', toggle = (e) => {
+	let classActive = document.querySelector('.status-check');
+	e.target.value = checkButton;
+	checkButton = !checkButton;
+	checkButton == true ? 
+	(classActive.classList.add('check-if-active'), classActive.innerHTML = 'Active') : 
+	(classActive.classList.remove('check-if-active'), classActive.innerHTML = 'Inactive')
+	console.log(checkButton);
+}, true);
+
+let timeButton = document.querySelector('.updated-class button').addEventListener('click', updateTime = (e) => {
+	const now = new Date();
+
+	let hours = now.getHours();
+	let minutes = now.getMinutes();
+	let seconds = now.getSeconds();
+
+	const timeOfDay = '';
+	hours > 12 ? timeofDay = 'PM' : timeofDay = 'AM';
+
+	hours < 10 ? hours = '0' + hours : hours;
+	minutes < 10 ? minutes = '0' + minutes : minutes;
+	seconds < 10 ? seconds = '0' + seconds : seconds;
+
+
+	dd = now.getDate();
+	mm = now.getMonth();
+	yyyy = now.getFullYear();
+
+	// if(dd < 10 || mm < 10) {
+	//   dd = '0' + dd;
+	//   mm = '0' + mm
+	// };
+
+	console.log(`Current time is: ${dd}/${mm}/${yyyy} ${hours} ${timeofDay} : ${minutes} : ${seconds}`);
+});
+
